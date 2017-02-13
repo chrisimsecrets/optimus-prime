13a858197b425dfaa21d83984c04e11e0eec6fd6
Mehedi Hassan <MehediDracula@gmail.com>
2017-02-14 00:15:28 +0600

    Add schedule

---
+++ app/Console/Commands/ProcessTasks.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\OptSchedul;
+use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
+
+class ProcessTasks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tasks:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run scheduled tasks.';
+
+    protected $schedule;
+
+    protected $carbon;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Schedule $schedule, Carbon $carbon)
+    {
+        $this->schedule = $schedule;
+
+        $this->carbon = $carbon;
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tasks = OptSchedul::all();
+        
+        foreach ($taks as $task) {
+            if ($this->carbon->parse($task->time)->) {
+                $postTime = $this->carbon->parse($tasks[0]->time);
+                $currentTime = $this->carbon->now();
+                
+                if ($postTime->lt($currentTime)) {
+                    // task will run here
+                }
+            }
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,8 +22,9 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
-        Commands\prappo::class,
-        Commands\OptimusPost::class,
+        // Commands\prappo::class,
+        // Commands\OptimusPost::class,
+        Commands\ProcessTasks::class,
     ];
 
     /**
@@ -34,42 +35,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('inspire')
-//                 ->hourly();
-        $schedule->command('optimus everyMinute')->everyMinute();
-
-        $schedule->command('optimus everyFiveMinutes')->everyFiveMinutes();
-
-        $schedule->command('optimus everyTenMinutes')->everyTenMinutes();
-
-        $schedule->command('optimus everyThirtyMinutes')->everyThirtyMinutes();
-
-        $schedule->command('optimus hourly')->hourly();
-
-        $schedule->command('optimus daily')->daily();
-
-        $schedule->command('optimus weekly')->weekly();
-        
-        $schedule->command('optimus monthly')->monthly();
-
-        $schedule->command('optimus quarterly')->quarterly();
-
-        $schedule->command('optimus yearly')->yearly();
-
-        $schedule->command('optimus fridays')->fridays();
-
-        $schedule->command('optimus saturdays')->saturdays();
-
-        $schedule->command('optimus sundays')->sundays();
-
-        $schedule->command('optimus mondays')->mondays();
-
-        $schedule->command('optimus tuesdays')->tuesdays();
-
-        $schedule->command('optimus wednesdays')->wednesdays();
-
-        $schedule->command('optimus thursdays')->thursdays();
-
-
+        $schedule->command('tasks:run')->everyMinute();
     }
 }
